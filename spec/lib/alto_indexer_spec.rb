f486c54cb6e9c410a90faf32e6527e3ab7047bbc
Chris Beer <cabeer@stanford.edu>
2017-12-11 17:47:23 -0800

    Add indexing test

--- spec/lib/alto_indexer_spec.rb
+++ spec/lib/alto_indexer_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 require 'alto_indexer'
 
 RSpec.describe AltoIndexer do
+  subject(:indexer) { described_class.new('x', 'y', File.read('spec/fixtures/bb018zb8894_04_0009.xml')) }
   describe '#to_solr' do
     it 'creates a solr document for indexing' do
-
+      expect(indexer.to_solr).to include id: 'x/y', druid: 'x', resource_id: 'y', ocrtext: start_with('George')
     end
   end
 end
