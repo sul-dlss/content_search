5fb1d15c2733ba0a85bbabdf9cc18eee399252b5
Jack Reed <phillipjreed@gmail.com>
2018-05-01 07:03:39 -0600

    add sidekiq check to okcomputer

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -2,3 +2,6 @@
 OkComputer.mount_at = false
 
 OkComputer::Registry.register "solr", OkComputer::SolrCheck.new(Search.client.uri.to_s.sub(/\/$/, ''))
+
+# Built-in Sidekiq check
+OkComputer::Registry.register 'feature-sidekiq', OkComputer::SidekiqLatencyCheck.new('default', 4.hours)
