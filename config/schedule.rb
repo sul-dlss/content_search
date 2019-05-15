3ee1bf4b4c90fed2ba60653695fb142ff3c23d3f
Jack Reed <phillipjreed@gmail.com>
2019-05-15 14:01:49 -0600

    Setup build suggest to be run nightly

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,3 +2,8 @@
 every '*/15 * * * *' do
   runner 'HarvestPurlFetcherJob.perform_now'
 end
+
+# Build suggest index (enabling autocomplete)
+every '0 3 * * *' do
+  runner 'BuildSuggestJob.perform_now'
+end
