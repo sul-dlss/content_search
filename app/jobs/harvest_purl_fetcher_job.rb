cfc11802fffc01105505c3ec2e4e5bb0cb11a999
Chris Beer <cabeer@stanford.edu>
2019-05-30 10:13:01 -0700

    Make incremental checkpoints for harvesting from purl-fetcher

--- app/jobs/harvest_purl_fetcher_job.rb
+++ app/jobs/harvest_purl_fetcher_job.rb
@@ -5,10 +5,18 @@ class HarvestPurlFetcherJob < ApplicationJob
   STATE_FILE = Rails.root + 'tmp' + "harvest_purl_fetcher_job_last_run_#{Rails.env}"
 
   def perform(first_modified = nil)
-    first_modified ||= File.read(STATE_FILE).strip if File.exist? STATE_FILE
-    perform_deletes(first_modified)
-    most_recent_timestamp = perform_indexes(first_modified)
-    write_state_file(most_recent_timestamp) if first_modified.nil? || (most_recent_timestamp > first_modified)
+    with_state_file do |f|
+      first_modified ||= f.read.strip
+      perform_deletes(first_modified)
+      perform_indexes(first_modified) do |most_recent_timestamp|
+        if most_recent_timestamp > first_modified
+          f.rewind
+          f.truncate(0)
+          f.puts(most_recent_timestamp)
+          first_modified = most_recent_timestamp
+        end
+      end
+    end
   end
 
   private
@@ -21,18 +29,28 @@ class HarvestPurlFetcherJob < ApplicationJob
     end
   end
 
+  # Queue up indexing jobs for content from purl fetcher
+  # @param [String] first_modified
+  # @yieldparam [Time] the timestamp of the most recently updated document (or the extent of the queried range)
   def perform_indexes(first_modified)
     client = PurlFetcher::Client::Reader.new('', 'purl_fetcher.first_modified' => first_modified)
-    last_record = nil
-    client.each do |record, _change, meta|
-      IndexFullTextContentJob.perform_later(record.druid)
-      last_record = meta
+    client.each_slice(1000) do |batch|
+      batch.each do |record, _change|
+        IndexFullTextContentJob.perform_later(record.druid)
+      end
+
+      ts = batch.map { |_, change| Time.zone.parse(change['updated_at']) if change['updated_at'] }.max
+      yield ts if ts
     end
 
-    last_record['range']['last_updated'] if last_record
+    yield client.range['last_updated']
   end
 
-  def write_state_file(timestamp)
-    File.open(STATE_FILE, 'w') { |f| f.puts timestamp }
+  def with_state_file
+    File.open(STATE_FILE, 'w') { |f| f.puts Time.zone.parse('1970-01-01T00:00:00Z') } unless File.exist? STATE_FILE
+    File.open(STATE_FILE, 'r+') do |f|
+      f.flock(File::LOCK_EX | File::LOCK_NB)
+      yield f
+    end
   end
 end
