89d68fd2d8ab44e5bcac97e8ca53323565484429
Justin Coyne <jcoyne@justincoyne.com>
2024-05-30 09:50:05 -0500

    Avoid sending out cron emails with unhelpful messages

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-job_type :runner,  "cd :path && :environment_variable=:environment bin/rails runner -e :environment ':task' :output"
+job_type :runner,  "cd :path && RAILS_LOG_LEVEL=warn :environment_variable=:environment bin/rails runner -e :environment ':task' :output"
 
 # Garbage collect the index
 every '15 * * * *' do
