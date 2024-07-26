2cea24b55b09202b31516c5971cfc80e065bcf79
Chris Beer <cabeer@stanford.edu>
2024-07-26 13:55:16 -0700

    Use cocina instead of public xml to drive content search indexing.

--- spec/models/purl_object/file_spec.rb
+++ spec/models/purl_object/file_spec.rb
@@ -3,14 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe PurlObject::File do
-  subject(:purl_file) { described_class.new('somedruid', file_xml_fragment) }
+  subject(:purl_file) { described_class.new('somedruid', file_metadata, fileset_metadata) }
 
-  let(:file_xml_fragment) { Nokogiri::XML.parse(xml).root }
-  let(:xml) { '<file id="92280263.xml" mimetype="application/xml" size="46424"></file>' }
+  let(:file_metadata) { { filename: '92280263.xml', hasMimeType: 'application/xml', size: 46424 }.with_indifferent_access }
+  let(:fileset_metadata) { { externalIdentifier: 'https://cocina.sul.stanford.edu/fileSet/abc123' }.with_indifferent_access }
 
   describe '#file_url' do
     context 'with a file with a space' do
-      let(:xml) { '<file id="Read Me" mimetype="application/xml" size="46424"></file>' }
+      let(:file_metadata) { { filename: 'Read Me', hasMimeType: 'application/xml', size: 46424 }.with_indifferent_access }
 
       it 'URI escapes the file name' do
         expect(purl_file.file_url).to eq 'https://stacks.stanford.edu/file/somedruid/Read+Me'
